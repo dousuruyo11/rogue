ea4b50daf57fdd509ae2daaba6f66aa444414221
dousuruyo <suruyo729@gmail.com>
2025-05-15 16:51:30 +0900

    deploy用に修正

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from "./pages/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
